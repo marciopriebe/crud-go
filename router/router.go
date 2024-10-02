27a0324d1238a6025926dad175dd0f8bd75992ab
marciopriebe <m-priebe@hotmail.com>
2024-10-02 00:25:44 +0000

    feat: add routes logic

--- router/router.go
+++ router/router.go
@@ -6,12 +6,13 @@ import (
 
 //
 func Initializer() {
+	
+	// Initializer Router
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	
+	initializeRoutes(router)
+
+	// Run server
 	router.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
 	
---
+++ router/routes.go
@@ -0,0 +1,49 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func initializeRoutes(router *gin.Engine)  {
+	v1 := router.Group("/api/v1")
+	{
+		// Show Opening
+		v1.GET("/opening", func (ctx *gin.Context){
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "GET Opening",
+			})
+		})
+
+		// Create
+		v1.POST("/opening", func (ctx *gin.Context){
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "POST Opening",
+			})
+
+		})
+
+		// DELETE
+		v1.DELETE("/opening", func (ctx *gin.Context){
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "DELETE Opening",
+			})
+		})
+		
+		//UPDATE
+		v1.PUT("/opening", func (ctx *gin.Context){
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "PUT Opening",
+			})
+		})
+
+		// SHOW ALL 
+		v1.GET("/openings", func (ctx *gin.Context){
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "GET openings",
+			})
+		})
+
+	}
+}
